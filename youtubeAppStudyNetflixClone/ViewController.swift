9e2a2fa323b413e0e9a953402d0bb7537cb9e370
ShaberAl <dsa_usa_clan_second_life@mail.ru>
2023-11-10 16:18:47 +0500

    Initial Commit

---
+++ youtubeAppStudyNetflixClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtubeAppStudyNetflixClone
+//
+//  Created by MacOS on 10.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
